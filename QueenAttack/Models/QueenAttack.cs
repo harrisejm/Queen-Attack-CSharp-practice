a86c192bf3d96e60b7cfa85dc522a29a8046e7cc
Eddie Harris and Swati Sahay <swati@>
2018-06-26 16:43:57 -0700

    add user interface

--- QueenAttack/Models/QueenAttack.cs
+++ QueenAttack/Models/QueenAttack.cs
@@ -27,3 +27,26 @@ public class Queen
   }
 }
 }
+class Leet
+{
+  static void Main()
+  {
+    Queen queenKill = new Queen();
+    Console.WriteLine (" Please enter queen x position");
+    string queenX = Console.ReadLine();
+    int intX = int.Parse(queenX)
+    Console.WriteLine (" Please enter queen y position");
+    string queenY = Console.ReadLine();
+    int intY = int.Parse(queenY)
+    Console.WriteLine (" Please enter piece x position");
+    string pieceX = Console.ReadLine();
+    int intPieceX = int.Parse(pieceX)
+    Console.WriteLine (" Please enter piece y position");
+    string pieceY = Console.ReadLine();
+    int intPieceY = int.Parse(pieceY)
+    Console.WriteLine(queenKill.isQueenKill(intX,intY,intPieceX,intPieceY
+     ));
+  }
+}
+
+}
